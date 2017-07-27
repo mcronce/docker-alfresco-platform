a40f9deb8bbc8b882fa034c4285f176b3e38564d
Mike Cronce <mike@quadra-tec.net>
2017-07-27 19:49:06 -0400

    assets/entrypoint.sh:  Handle variables in alfresco-global.properties

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -5,5 +5,16 @@ sed -i --follow-symlinks \
 	-e "s/%AJP_PORT%/${TOMCAT_HTTP_PORT:-8080}/" \
 '/usr/local/tomcat/conf/server.xml';
 
+sed -i --follow-symlinks \
+	-e "s/%DB_USER%/${DB_USER:-alfresco}/" \
+	-e "s/%DB_PASSWORD%/${DB_PASSWORD:-alfresco}/" \
+	-e "s/%DB_NAME%/${DB_NAME:-alfresco}/" \
+	-e "s/%DB_HOST%/${DB_HOST:-postgres}/" \
+	-e "s/%DB_PORT%/${DB_PORT:-5432}/" \
+	-e "s/%SOLR_HOST%/${SOLR_HOST:-solr}/" \
+	-e "s/%SOLR_HTTP_PORT%/${SOLR_HTTP_PORT:-8080}/" \
+	-e "s/%SOLR_HTTPS_PORT%/${SOLR_HTTPS_PORT:-8443}/" \
+'/usr/local/tomcat/webapps/alfresco/WEB-INF/classes/alfresco-global.properties';
+
 /usr/local/tomcat/bin/catalina.sh $@;
 
