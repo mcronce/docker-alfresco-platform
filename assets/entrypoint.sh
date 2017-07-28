9fa7e0141d5d1cb1b05650b07b05be19e84830f1
Mike Cronce <mike@quadra-tec.net>
2017-07-27 20:18:28 -0400

    assets/entrypoint.sh:  Whoops, that should be HTTP_PORT not AJP_PORT

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -2,7 +2,7 @@
 
 sed -i --follow-symlinks \
 	-e "s/%BASE_PORT%/${TOMCAT_BASE_PORT:-8005}/" \
-	-e "s/%AJP_PORT%/${TOMCAT_HTTP_PORT:-8080}/" \
+	-e "s/%HTTP_PORT%/${TOMCAT_HTTP_PORT:-8080}/" \
 '/usr/local/tomcat/conf/server.xml';
 
 sed -i --follow-symlinks \
